5f8f0a08fa724a95a9ce3f5ef43c6c511b882722
Jenny Belanger <jbelangr@gmail.com>
2020-07-04 11:39:28 -0400

    Convert inline SVG to files

--- src/js/components/Search.jsx
+++ src/js/components/Search.jsx
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { ReactComponent as ClearIcon } from '../../svg/x.svg';
+import { ReactComponent as SearchIcon } from '../../svg/search.svg';
 
 export default class Search extends React.Component {
 	state = {
@@ -59,12 +61,21 @@ export default class Search extends React.Component {
 		const inputLabel = `Filter ${this.props.type}`;
 		const buttonClass = `postfix button--icon button--${this.isSearching() ? 'clear' : 'search'}`;
 		const buttonText = this.isSearching() ? `Clear ${this.props.type} filter` : `Search ${this.props.type}`;
+		let Icon;
+		if (this.isSearching()) {
+			Icon = ClearIcon;
+		} else {
+			Icon = SearchIcon;
+		}
 
 		return (
 			<form onSubmit={this.onSubmit}>
 				<p className="search">
 					<input aria-label={inputLabel} className="prefix" placeholder={inputLabel} type="text" value={this.state.value} onChange={this.filter} />
-					<button className={buttonClass} type="button" onClick={this.onClick}>{buttonText}</button>
+					<button className={buttonClass} type="button" onClick={this.onClick}>
+						<Icon />
+						<span>{buttonText}</span>
+					</button>
 				</p>
 			</form>
 		);
